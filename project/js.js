544349034d72c8f4b0f5744bb43b482427b5559e
Dias Almakham <91239735+DiasAlmakhan@users.noreply.github.com>
2021-10-31 23:38:05 +0600

    Delete project directory

--- project/js.js
+++
@@ -1,6 +0,0 @@
-const shareButton = document.querySelector("#share-btn");
-const shareBox = document.querySelector("#share-box");
-
-shareButton.addEventListener("click", () => {
-	shareBox.classList.toggle("active");
-});
